ac8618b79db2d07ee4ca3680689856a1b3f1add2
shejaeddy <shejaeddy05@gmail.com>
2020-03-27 21:59:00 +0200

    feature(forgot-password): user request for a reset password link [Finishes #172007260]

--- politico/src/router/index.js
+++ politico/src/router/index.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../components/pages/index';
 import Login from '../components/pages/auth/login';
 import SignUp from '../components/pages/auth/signup';
+import ForgotPassword from '../components/pages/auth/forgotPassword';
 
 const Routes = () => {
   return (
@@ -10,6 +11,7 @@ const Routes = () => {
       <Route path="/" exact component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/forgot-password" component={ForgotPassword} />
     </Switch>
   );
 };
